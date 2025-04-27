d530685cd635bf38bdbd3e601d05f9fc88c45802
vivekananda7583 <22951a04p9@iare.ac.in>
2025-04-27 12:16:33 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const app=express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cookieParser);
+app.use(cookieParser());
 const corsoption = {
     origin: 'http://localhost:5000',
     credentials: true,
